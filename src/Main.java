35f177679176044568e6755c8ba24ab934178659
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-07-26 09:12:30 -0300

    mentoria curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso novo= new Curso();
+
+        novo.setTitulo("Back-End");
+        novo.setDescricao("Curso voltado para o back end na linguagem java");
+        novo.setCargaHoraria(48);
+
+        System.out.println(novo);
+
+        Mentoria novaMent= new Mentoria();
+
+        novaMent.setTitulo("Intesivo Java");
+        novaMent.setDescricao("Melhor boot camp");
+        novaMent.setData(LocalDate.of(2025, 12, 01));
+        System.out.println(novaMent);
+        
     }
 }
