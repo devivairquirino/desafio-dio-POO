42e1f7ff548b851979f78d881650718536c963eb
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-08-07 21:24:36 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.BootCamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -20,5 +22,29 @@ public class Main {
         novaMent.setData(LocalDate.of(2025, 12, 01));
         System.out.println(novaMent);
         
+
+        BootCamp bootcamp = new BootCamp();
+        bootcamp.setNome("BootCamp Java Developer");
+        bootcamp.setDescricao("Descrição BootCamp Java Developer");
+        bootcamp.getConteudo().add(novo);
+        bootcamp.getConteudo().add(novaMent);
+        System.out.println("---------------");
+
+    
+        Dev devIvair = new Dev();
+        devIvair.setNome("Ivair");
+        devIvair.inscreverBootCamp(bootcamp);
+        System.out.println("**Conteudos Inscritos" + devIvair.getConteudoInscritos());
+        devIvair.progredir();
+        System.out.println("**Conteudos Concluidos" + devIvair.getConteudoConcluidos());
+
+        System.out.println("___");
+
+        Dev devKeke = new Dev();
+        devKeke.setNome("Kethellen");
+        devKeke.inscreverBootCamp(bootcamp);
+        System.out.println("**Conteudos Inscritos" + devKeke.getConteudoInscritos());
+        devKeke.progredir();
+        System.out.println("**Conteudos Concluidos" + devKeke.getConteudoConcluidos());
     }
 }
--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,7 +1,7 @@
 package br.com.dio.desafio.dominio;
 
 public abstract class Conteudo {
-    protected static  final double XP_PADRAO= 100;
+    protected static  final double XP_PADRAO= 100d;
 
     private String titulo;
     private String descricao;
